14446bdee6b758d8330b3ec00cb8f612ce03e8d4
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2022-04-08 15:53:08 +0700

    First Commit W9

---
+++ Latihan 7.1/src/Animal.java
@@ -0,0 +1,8 @@
+// Aini Nurul Azizah
+
+public class Animal {
+	public void sound() {
+		System.out.println("Animal is making a sound");
+	}
+}
+
---
+++ Latihan 7.3/src/Volunteer.java
@@ -0,0 +1,13 @@
+// Aini Nurul Azizah
+
+public class Volunteer extends StaffMember {
+	
+	public Volunteer(String eName, String eAddress, String ePhone) {
+		super(eName, eAddress, ePhone);
+	}
+	
+	public double pay() {
+		return 0.0;
+	}
+}
+
